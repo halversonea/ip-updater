d971b40bc6a8d40b496df886903bef64ed2a1a02
Eric Halverson <ehalverson@gmail.com>
2023-02-16 07:48:04 -0500

    Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ip-updater
+
+Create a file `auth.sh` that can only be read by the user running the script with your credentials:
+
+```sh
+USER="USERCREDS"
+PASS="PASSCREDS"
+```
+
+Update the file `update-ip.sh` with your hostname then setup the script to run in cron.
--- update-ip.sh
+++ update-ip.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# https://support.google.com/domains/answer/6147083?hl=en
-
 source auth.sh
-HOST="hostname.network"
+HOST="YOUR_HOSTNAME"
+
+date
 
 CUR_IP=$(curl -s "https://domains.google.com/checkip")
-#CUR_IP="0.0.0.0"
-REG_IP=$(nslookup hostname.network | grep Address: | grep -v 192.168 | cut -d' ' -f2)
+REG_IP=$(nslookup $HOST | grep -A 1 $HOST | grep Address: | cut -d' ' -f2)
 
 if [ "$CUR_IP" != "$REG_IP" ]; then
     echo "Mismatch. Updating DNS record from $REG_IP to $CUR_IP"
+    curl "https://${USER}:${PASS}@domains.google.com/nic/update?hostname=${HOST}&myip=${CUR_IP}"
 else
     echo "Match. Record has $REG_IP and current IP is $CUR_IP"
 fi
-
-# Update DNS Record
-#curl -v "https://${USER}:${PASS}@domains.google.com/nic/update?hostname=${HOST}&myip=${CUR_IP}"
