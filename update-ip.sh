404611c28d3f67428795c791a6cb81e51bb6c7c8
Eric Halverson <ehalverson@gmail.com>
2024-11-25 15:09:31 -0500

    Switch from Google Domains to Namecheap

--- update-ip.sh
+++ update-ip.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-source PATH/TO/FILE/auth.sh
-HOST="YOUR_HOSTNAME"
+PASS="PASS"
+DOMAIN="DOMAINNAME"
+HOST="@"
 
 date
 
-CUR_IP=$(curl -s "https://domains.google.com/checkip")
+CUR_IP=$(curl -s ifconfig.io -4)
 REG_IP=$(nslookup $HOST | grep -A 1 $HOST | grep Address: | cut -d' ' -f2)
 
 if [ "$CUR_IP" != "$REG_IP" ]; then
     echo "Mismatch. Updating DNS record from $REG_IP to $CUR_IP"
-    curl "https://${USER}:${PASS}@domains.google.com/nic/update?hostname=${HOST}&myip=${CUR_IP}"
+    curl "https://dynamicdns.park-your-domain.com/update?host=${HOST}&domain=${DOMAIN}&password=${PASS}&ip=${CUR_IP}"
 else
     echo "Match. Record has $REG_IP and current IP is $CUR_IP"
 fi
